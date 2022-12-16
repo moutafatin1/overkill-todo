70fdfdbedd6f59860f815c14bf9bb9f2bca6e863
Oussama Moutafatin <moutafatin3@gmail.com>
2022-12-16 20:48:03 +0100

    little transition

--- src/components/TabItem.tsx
+++ src/components/TabItem.tsx
@@ -12,8 +12,8 @@ export const TabItem = ({ name, active, setSelectedTab }: TabItemProps) => {
       onClick={() => setSelectedTab(name)}
       className={fn(
         "font-medium capitalize text-gray-700",
-        "relative  after:absolute after:right-1/2 after:top-8 after:hidden after:w-24 after:translate-x-1/2 after:rounded-t-xl after:border-b-8 after:border-purple-500 after:content-['']",
-        active && "text-purple-500 after:block"
+        "relative  after:absolute after:right-1/2 after:top-8 after:w-24 after:translate-x-1/2 after:rounded-t-xl after:border-b-8 after:border-purple-500 after:opacity-0 after:transition-all after:duration-300 after:content-['']",
+        active && "text-purple-500  after:opacity-100"
       )}
     >
       {name}
