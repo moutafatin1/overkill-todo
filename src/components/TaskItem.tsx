6d2e0f326f11f536f20d3fd623461a7db0431bb4
Oussama Moutafatin <moutafatin3@gmail.com>
2022-12-16 19:22:30 +0100

    delete task

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,11 +1,17 @@
+import { HiOutlineTrash } from "react-icons/hi";
 import type { Task } from "../pages/tasks";
 
 type TaskItemProps = {
   task: Task;
   updateTaskStatus: (task: Task) => void;
+  deleteTaskById: (id: number) => void;
 };
 
-export const TaskItem = ({ task, updateTaskStatus }: TaskItemProps) => {
+export const TaskItem = ({
+  task,
+  updateTaskStatus,
+  deleteTaskById,
+}: TaskItemProps) => {
   return (
     <li className="flex items-center gap-4">
       <input
@@ -15,6 +21,12 @@ export const TaskItem = ({ task, updateTaskStatus }: TaskItemProps) => {
         onChange={() => updateTaskStatus(task)}
       />
       <span className="text-lg font-medium text-gray-800">{task.text}</span>
+      <button
+        onClick={() => deleteTaskById(task.id)}
+        className="ml-auto rounded-xl p-1 transition-all hover:opacity-80 focus:ring-1 focus:ring-red-500 active:scale-90"
+      >
+        <HiOutlineTrash className="text-3xl text-red-500" />
+      </button>
     </li>
   );
 };
--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -3,14 +3,24 @@ import { TaskItem } from "./TaskItem";
 
 type TasksListProps = {
   tasks: Task[];
-  updateTaskStatus : (task:Task) => void
+  updateTaskStatus: (task: Task) => void;
+  deleteTaskById: (id: number) => void;
 };
 
-export const TasksList = ({ tasks,updateTaskStatus }: TasksListProps) => {
+export const TasksList = ({
+  tasks,
+  updateTaskStatus,
+  deleteTaskById,
+}: TasksListProps) => {
   return (
     <ul className="space-y-4">
       {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} updateTaskStatus={updateTaskStatus} />
+        <TaskItem
+          key={task.id}
+          task={task}
+          updateTaskStatus={updateTaskStatus}
+          deleteTaskById={deleteTaskById}
+        />
       ))}
     </ul>
   );
