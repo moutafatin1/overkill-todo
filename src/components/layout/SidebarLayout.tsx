4ef36deb684e1879ebf9c37612c2b4295ad3fe99
Oussama Moutafatin <moutafatin3@gmail.com>
2022-12-17 09:35:56 +0100

    add mobile nav to open sidebar

--- src/components/layout/SidebarLayout.tsx
+++ src/components/layout/SidebarLayout.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 import { Sidebar } from "../common/Sidebar/Sidebar";
 
 type SidebarLayoutProps = {
@@ -14,6 +15,14 @@ export const SidebarLayout = ({ children }: SidebarLayoutProps) => {
   return (
     <>
       <Sidebar closeSidebar={closeSidebar} isOpen={isOpen} />
+      <nav className="bg-slate-300 p-4 shadow-lg md:hidden">
+        <button
+          onClick={() => setIsOpen(true)}
+          className="rounded-xl p-1 focus:outline-none focus:ring-1 focus:ring-purple-700"
+        >
+          <AiOutlineMenu className="text-2xl text-purple-700 transition-opacity hover:opacity-80" />
+        </button>
+      </nav>
       <main className="md:pl-72">{children}</main>
     </>
   );
