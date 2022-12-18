a175208e6f23cbee790745bfbec2dda4c49ce1ba
Oussama Moutafatin <moutafatin3@gmail.com>
2022-12-18 10:22:42 +0100

    show option button only when hover

--- src/components/tasks/FoldersMenu.tsx
+++ src/components/tasks/FoldersMenu.tsx
@@ -1,10 +1,13 @@
 import { Disclosure } from "@headlessui/react";
+import { useState } from "react";
+import { FaEllipsisV } from "react-icons/fa";
 import { fn } from "../../utils/fn";
 import { trpc } from "../../utils/trpc";
 import { Spinner } from "../common";
 import { CreateNewFolder } from "./CreateNewFolder";
 
 export const FoldersMenu = () => {
+  const [showOption, setShowOption] = useState("");
   const { data: folders, isLoading, error } = trpc.folder.all.useQuery();
   if (isLoading) return <Spinner show={isLoading} delay={400} />;
   if (error) return <p> {error.message}</p>;
@@ -20,26 +23,37 @@ export const FoldersMenu = () => {
         <Disclosure key={folder.id} as="div" className="space-y-1 p-2">
           {({ open }) => (
             <>
-              <Disclosure.Button
-                className={fn(
-                  false
-                    ? "bg-gray-900 text-white"
-                    : " text-gray-300 hover:bg-gray-700 hover:text-gray-100",
-                  "group flex w-full items-center rounded-md py-2 pr-2 text-left text-sm font-medium focus:outline-none focus:ring-2 focus:ring-purple-500"
-                )}
+              <div
+                className="flex items-center gap-2"
+                onMouseEnter={() => setShowOption(folder.id)}
+                onMouseLeave={() => setShowOption("")}
               >
-                <svg
+                <Disclosure.Button
                   className={fn(
-                    open ? "rotate-90 text-gray-400" : "text-gray-300",
-                    "mr-2 h-5 w-5 flex-shrink-0 transform transition-colors duration-150 ease-in-out group-hover:text-gray-400"
+                    false
+                      ? "bg-gray-900 text-white"
+                      : " text-gray-300 hover:bg-gray-700 hover:text-gray-100",
+                    "group flex w-full items-center rounded-md py-2 pr-2 text-left text-sm font-medium focus:outline-none focus:ring-2 focus:ring-purple-500"
                   )}
-                  viewBox="0 0 20 20"
-                  aria-hidden="true"
                 >
-                  <path d="M6 6L14 10L6 14V6Z" fill="currentColor" />
-                </svg>
-                {folder.name}
-              </Disclosure.Button>
+                  <svg
+                    className={fn(
+                      open ? "rotate-90 text-gray-400" : "text-gray-300",
+                      "mr-2 h-5 w-5 flex-shrink-0 transform transition-colors duration-150 ease-in-out group-hover:text-gray-400"
+                    )}
+                    viewBox="0 0 20 20"
+                    aria-hidden="true"
+                  >
+                    <path d="M6 6L14 10L6 14V6Z" fill="currentColor" />
+                  </svg>
+                  {folder.name}
+                </Disclosure.Button>
+                {showOption === folder.id && (
+                  <button className="ml-auto rounded-lg p-1 text-gray-200 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-1 focus:ring-purple-400">
+                    <FaEllipsisV className="text-xl" />
+                  </button>
+                )}
+              </div>
               <Disclosure.Panel className="space-y-1">
                 {folder.List.map((list) => (
                   <Disclosure.Button
