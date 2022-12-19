3fa51bb050a3d23eb1ef01936407c9aeafc347d2
Oussama Moutafatin <moutafatin3@gmail.com>
2022-12-19 09:58:09 +0100

    new list endpoint

---
+++ src/server/trpc/router/list.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+import { protectedProcedure, router } from "../trpc";
+
+export const listRouter = router({
+  new: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        folderId: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.list.create({
+        data: {
+          name: input.name,
+          folderId: input.folderId,
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
+});
